dacab63c00fe03abc6b24d7786caacf6990a9c5d
Tope <user@Topes-MacBook-Pro.local>
2024-02-21 18:14:03 +0100

    initiate payment feature added

--- src/modules/payment/payment.resolver.ts
+++ src/modules/payment/payment.resolver.ts
@@ -1,7 +1,19 @@
-import { Resolver } from '@nestjs/graphql';
+import { Mutation, Resolver, Args } from '@nestjs/graphql';
 import { PaymentService } from './payment.service';
 
 @Resolver()
 export class PaymentResolver {
   constructor(private readonly paymentService: PaymentService) {}
+
+  @Mutation(() => String)
+  async initiatePayment(
+    @Args('amount') amount: number,
+    @Args('email') email: string,
+  ): Promise<string> {
+    const paymentResponse = await this.paymentService.makePayment(
+      amount,
+      email,
+    );
+    return paymentResponse.data.authorization_url;
+  }
 }
