9cfb549e57baef946f4b08b915a1fe26b138434e
Josh-Savvy <joshsavyy007@gmail.com>
2024-02-27 22:25:41 +0100

    updated mentor create input validations

--- src/modules/mentor/types/mentor.type.ts
+++ src/modules/mentor/types/mentor.type.ts
@@ -1,16 +1,11 @@
-import { AvailailabilityDays } from '../enums/mentor.enum';
-
 export type Skill = {
   skill_name: string;
-
   years_of_exp: number;
 };
 
 export type WorkExperience = {
   company: string;
-
   job_role: string;
-
   description: string;
   from_year: Date;
   to_year: Date;
@@ -18,30 +13,22 @@ export type WorkExperience = {
 
 export type PastProjects = {
   company: string;
-
   job_role: string;
-
   description: string;
 };
 
 export type Education = {
   school: string;
-
   credential_type: string;
-
   course_of_study: string;
-
   from_year: Date;
-
   to_year: Date;
 };
 
 export type Certification = {
   organization: string;
-
   title: string;
-
-  year: Date;
+  year: string;
 };
 
 export type UserAvailability = {
