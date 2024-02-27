76c28748a7394709630100c77dcd0acfa88517b7
Josh-Savvy <joshsavyy007@gmail.com>
2024-02-27 20:32:12 +0100

    patch: register input sanitization

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,35 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import * as passport from 'passport';
 import { AppModule } from './app.module';
-import * as graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.js';
+import {
+  ClassSerializerInterceptor,
+  ValidationPipe,
+  BadRequestException,
+} from '@nestjs/common';
+import { useContainer } from 'class-validator';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(passport.initialize());
   app.enableCors();
-  app.use(
-    '/graphql',
-    graphqlUploadExpress({ maxFileSize: 100000000, maxFiles: 10 }),
+  const reflector = app.get(Reflector);
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
+  // Format dto validator error messages
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      exceptionFactory: (errors) => {
+        const errorMessage = errors
+          .flatMap((error) => Object.values(error.constraints))
+          .join(', ');
+        return new BadRequestException(errorMessage);
+      },
+    }),
   );
-  const PORT = 3000;
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+  const PORT = process.env.NODE_PORT || 10005;
   await app.listen(PORT);
 }
 bootstrap();
