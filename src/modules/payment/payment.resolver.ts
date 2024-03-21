c58b076c9bafca4a353237511d97c65f0edaa111
Josh-Savvy <joshsavyy007@gmail.com>
2024-03-22 00:30:19 +0100

    updated the payment service

--- src/modules/payment/payment.resolver.ts
+++ src/modules/payment/payment.resolver.ts
@@ -1,19 +1,16 @@
 import { Mutation, Resolver, Args } from '@nestjs/graphql';
 import { PaymentService } from './payment.service';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from '../auth/guards/gql-auth.guard';
 
 @Resolver()
 export class PaymentResolver {
   constructor(private readonly paymentService: PaymentService) {}
 
+  @UseGuards(GqlAuthGuard)
   @Mutation(() => String)
-  async initiatePayment(
-    @Args('amount') amount: number,
-    @Args('email') email: string,
-  ): Promise<string> {
-    const paymentResponse = await this.paymentService.makePayment(
-      amount,
-      email,
-    );
+  async initiatePayment(@Args('amount') amount: number): Promise<string> {
+    const paymentResponse = await this.paymentService.makePayment(amount);
     return paymentResponse.data.authorization_url;
   }
 }
--- src/modules/payment/payment.service.ts
+++ src/modules/payment/payment.service.ts
@@ -1,42 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { REQUEST } from '@nestjs/core';
 import axios from 'axios';
+import { User } from '../user/entities/user.entity';
 
 @Injectable()
 export class PaymentService {
+  private readonly logger = new Logger(PaymentService.name);
   private paystackBaseUrl = 'https://api.paystack.co';
 
-  constructor(private configService: ConfigService) {}
+  constructor(
+    @Inject(REQUEST) private readonly request: { req: { user: User } },
+    private configService: ConfigService,
+  ) {}
 
   /**
    * TODO: Connect payment gateway
    */
-  async makePayment(amount: number, email: string): Promise<any> {
+  async makePayment(amount: number): Promise<any> {
     const secretKey = this.configService.get('PAYSTACK_SECRET_KEY');
     const url = `${this.paystackBaseUrl}/transaction/initialize`;
-
+    const user = this.request.req.user;
+    const payload = {
+      amount: amount * 100,
+      email: user.email,
+      currency: 'NGN',
+    };
     try {
-      const response = await axios.post(
-        url,
-        {
-          amount: amount * 100,
-          email: email,
-          currency: 'NGN',
+      const response = await axios.post(url, payload, {
+        headers: {
+          Authorization: `Bearer ${secretKey}`,
+          'Content-Type': 'application/json',
         },
-        {
-          headers: {
-            Authorization: `Bearer ${secretKey}`,
-            'Content-Type': 'application/json',
-          },
-        },
-      );
-
+      });
+      console.log({ data: response.data, response });
       return response.data;
     } catch (error) {
       console.error('Payment error:', error);
-      throw new Error('Payment initiation failed');
+      const err = new Error('Payment initiation failed');
+      this.logger.error(error, err.stack);
+      throw err;
     }
   }
+
+  // Todo: verifyPayment
+
   // async addNewCard() {}
   // async getCard() {}
   // async getCards() {}
